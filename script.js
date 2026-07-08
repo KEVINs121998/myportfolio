c06a77b7c0d09de7564ae66cb098321ec74f0686
kevin <kevinshah12jan@gmail.com>
2026-07-08 17:13:37 +0530

    dark button changed

--- script.js
+++ script.js
@@ -2,16 +2,15 @@ const toggle = document.getElementById("theme-btn");
 
 if (localStorage.getItem("theme") === "dark") {
     document.body.classList.add("dark-mode");
-    toggle.checked = true;
 }
 
-toggle.addEventListener("change", function () {
+toggle.addEventListener("click", function () {
     document.body.classList.toggle("dark-mode");
 
     if (document.body.classList.contains("dark-mode")) {
-        localStorage.setItem("theme", "dark");
+       toggle.textContent="Light Mode"
     } else {
-        localStorage.setItem("theme", "light");
+        toggle.textContent="Dark Mode"
     }
 });
 
